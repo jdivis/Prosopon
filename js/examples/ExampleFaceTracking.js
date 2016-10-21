4c23e3418add463750c05fb7e79fb84597878ec2
jdivis <jasondivis@gmail.com>
2016-10-21 08:51:40 -0500

    position mustache better

--- js/examples/ExampleFaceTracking.js
+++ js/examples/ExampleFaceTracking.js
@@ -65,10 +65,10 @@ function calcFinalHatValues(){
 }
 
 function calcFinalMustacheValues(){
-	mustacheFinalW = roundedW * 0.333;
-	mustacheFinalH = roundedW * 0.5;
+	mustacheFinalW = roundedW * 0.5;
+	mustacheFinalH = roundedW * 0.2;
 	mustacheFinalX = faceShape.points[41].x - (mustacheFinalW/2);
-	mustacheFinalY = faceShape.points[41].y - (mustacheFinalH/2);	
+	mustacheFinalY = faceShape.points[41].y - (mustacheFinalH/2) + 10;	
 }
 
 function calcFinalMaskValues(){
